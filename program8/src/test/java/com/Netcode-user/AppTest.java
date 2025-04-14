5bd12ebbc2c9b1b1557630910239c8cd2918dd2e
Annapoorna SJ <anujagadeeshmurthy@gmail.com>
2025-04-14 11:39:29 +0530

    Update AppTest.java

--- program8/src/test/java/com/Netcode-user/AppTest.java
+++ program8/src/test/java/com/Netcode-user/AppTest.java
@@ -1,8 +1,7 @@
-package com.netcode_user;  // OR rename to something like com.netcode.user
-
+package com.netcode_user; 
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins!");
+        System.out.println("Build success! Hello from Jenkins.");
     }
 }
